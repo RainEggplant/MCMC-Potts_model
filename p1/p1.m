b9d3d4ed53bcb10609f333813f328bea519bed67
RainEggplant <i@raineggplant.com>
2019-11-14 17:06:15 +0800

    Skip initial steps

--- p1/p1.m
+++ p1/p1.m
@@ -1,9 +1,12 @@
 clear;
 %% 常数、参数定义
 N = 100000; % 迭代次数
+SKIP_INITIAL_FACTOR = 0.2;
 mu = [5; 10];
 sigma = [1, 1; 1, 4];
 P = @(x) mvnpdf(x, mu, sigma);
+
+n_skip = round(N * SKIP_INITIAL_FACTOR);
 
 %% 计算理论值
 s0 = mvnrnd(mu, sigma, N)';
@@ -34,12 +37,12 @@ end
 % 与理论值作图对比
 figure;
 subplot(1, 2, 1);
-histogram2(s0(1, :), s0(2, :));
+histogram2(s0(1, n_skip:end), s0(2, n_skip:end));
 title('理论值');
 subplot(1, 2, 2);
-histogram2(s1(1,:), s1(2,:));
+histogram2(s1(1, n_skip:end), s1(2, n_skip:end));
 title('均匀分布举荐-MH');
-corr_s1 = corrcoef(s1(1, :), s1(2, :));
+corr_s1 = corrcoef(s1(1, n_skip:end), s1(2, n_skip:end));
 disp(['均匀分布举荐-MH 估计相关系数： ' ,num2str(corr_s1(1, 2))]);
 
 %% 计算举荐分布为二维高斯分布
@@ -68,12 +71,12 @@ end
 % 与理论值作图对比
 figure;
 subplot(1, 2, 1);
-histogram2(s0(1, :), s0(2, :));
+histogram2(s0(1, n_skip:end), s0(2, n_skip:end));
 title('理论值');
 subplot(1, 2, 2);
-histogram2(s2(1,:), s2(2,:));
+histogram2(s2(1, n_skip:end), s2(2, n_skip:end));
 title('二维高斯分布举荐-MH ');
-corr_s2 = corrcoef(s2(1, :), s2(2, :));
+corr_s2 = corrcoef(s2(1, n_skip:end), s2(2, n_skip:end));
 disp(['二维高斯分布举荐-MH 估计相关系数： ' ,num2str(corr_s2(1, 2))]);
 
 %% 整体对比
@@ -81,10 +84,10 @@ figure;
 subplot(1, 2, 1);
 hold on;
 title('理论分布与均匀分布举荐-MH 采样');
-plot(s0(1, :), s0(2, :), '.b');
-plot(s1(1, :), s1(2, :), '.r');
+plot(s0(1, n_skip:end), s0(2, n_skip:end), '.b');
+plot(s1(1, n_skip:end), s1(2, n_skip:end), '.r');
 subplot(1, 2, 2);
 hold on;
 title('理论分布与二维高斯分布举荐-MH 采样');
-plot(s0(1, :), s0(2, :), '.b');
-plot(s2(1, :), s2(2, :), '.r');
+plot(s0(1, n_skip:end), s0(2, n_skip:end), '.b');
+plot(s2(1, n_skip:end), s2(2, n_skip:end), '.r');
